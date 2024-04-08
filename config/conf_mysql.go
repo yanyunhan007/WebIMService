d6f3328d113fdabe5fedc9d24d402bd387e9a79e
13291 <1329159231@qq.com>
2024-04-08 16:41:36 +0800

    init

---
+++ config/conf_mysql.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"fmt"
+)
+
+type MySQL struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	DB       string `yaml:"db"`
+	User     string `yaml:"users"`
+	Password string `yaml:"password"`
+	LogLevel string `yaml:"log_level"` //日志等级
+}
+
+func (ms *MySQL) Dsn() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local", ms.User, ms.Password, ms.Host, ms.Port, ms.DB)
+}
